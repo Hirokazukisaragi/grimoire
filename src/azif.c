854a0873571ce7257bd5294163f07e4a5e2fdfe2
hirokazu <hirokazu@free-of-x.com>
2017-09-01 18:38:19 +0900

    implement random and input and more...

--- src/azif.c
+++ src/azif.c
@@ -1,48 +1,50 @@
 /*
-one line to give the program's name and an idea of what it does.
-Copyright (C) 2017  name of hirokazu
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    any later version.
 
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 2
-of the License, or (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
+#include <time.h>
 
 char EXITFLAG = 0;
 const long MAX_LINE_LEN = 80;
-const long MAX_RAW = 20;
+const long MAX_ROW = 20;
 char **command;
 //char *inputLine();
 void inputLine();
 char outPut(char *command);
 //stack pointer
 long sp = 0;
+char PANIC = 0;
 int VALUE[26];
+char input_string[65554];
 char VAL_FLAG = 0;
+FILE *fp;
+int retvalnum(char ch);
 int main(int argc,char *argv[]){
   int i = 0;
   for(i = 0;i < 26;i++){
     VALUE[i] = 0;
   }
+  srand((unsigned)time(NULL));
   command = malloc(MAX_LINE_LEN * sizeof(char *));
-  for(i = 0;i < MAX_RAW;i++){
+  for(i = 0;i < MAX_ROW;i++){
     command[i] = malloc(sizeof(char) * MAX_LINE_LEN);
   }
-      
+  if(argc==1){
   while(1){
     //command[sp] = inputLine();
     inputLine();
@@ -52,8 +54,23 @@ int main(int argc,char *argv[]){
     }
     sp++;
   }
-  
-  for(i = 0;i < MAX_RAW;i++){
+
+  }
+  /***********
+   *command mode
+   ***********/
+  if(argc!=1){
+    fp = fopen(argv[1],"r");
+    while(1){
+      fgets(command[sp],MAX_LINE_LEN,fp);
+      outPut(command[sp]);
+      if(EXITFLAG){
+        break;
+      }
+      sp++;
+    }
+  }
+  for(i = 0;i < MAX_ROW;i++){
     free(command[i]);
   }
   
@@ -61,7 +78,7 @@ int main(int argc,char *argv[]){
   free(command);
   return 0;
 }
-	
+//input command
 void inputLine(){
   fgets(command[sp],MAX_LINE_LEN,stdin);
 //return command[sp];
@@ -76,9 +93,11 @@ char outPut(char *command){
     command[i] = toupper(command[i]);
   }
   i = 0;
+  //ASSIGN
   if(command[i] == '$'){
     i++;
-    valnum = command[i] - 'A';
+    //valnum = command[i] - 'A';
+    valnum = retvalnum(command[i]);
     i++;
     if(command[i] == '='){
       i++;
@@ -94,18 +113,33 @@ char outPut(char *command){
     i++;
   }
   i=0;
+  //PRINT value
   if(command[i] == '!'){
     i++;
-    printval = command[i] - 'A';
+    printval = retvalnum(command[i]);
     printf("%d\n",VALUE[printval]);
   }
   if(command[i] == '?'){
     i++;
-    valnum = command[i] - 'A';
+    valnum = retvalnum(command[i]);
     cond = VALUE[valnum];
     if(!cond){
       while(command[i++] != '\n');
     }
+  }
+  //Random number
+  if(command[i] == '@'){
+    i++;
+    valnum = retvalnum(command[i]);
+    VALUE[valnum] = rand();
+  }
+  //INPUT only STRING!
+  if(command[i] == '&'){
+    fgets(input_string,MAX_LINE_LEN,stdin);
+  }
+  //PRINT string
+  if(command[i] == '#'){
+    printf("%s\n",input_string);
   }
   //debug mode
   /*
@@ -114,10 +148,18 @@ char outPut(char *command){
     printf("%c=",j+'A');
     printf("%d\n",VALUE[j]);
   }
-  */
   printf("%s",command);
-  if(!strcmp(command,"END\n")){
+  */
+  if(!strcmp(command,"END\n") || !strcmp(command,"END")){
     EXITFLAG = 1;
   }
   return 0;
+}
+int retvalnum(char ch){
+  int retnum = ch - 'A';
+  if(retnum > 25 || retnum < 0){
+    printf("value error\n");
+    exit(1);
+  }
+  return retnum;
 }
